0363677856bc6744730ac3d323bdad8f1d7d5fca
Cita Wafa Atiah <23109692@siswa.um.edu.my>
2025-01-10 00:40:14 +0800

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,18 +1,25 @@
+
+import java.util.*;
+
 public class Task {
+    
     private String title;
     private String description;
     private String dueDate;
     private String category;
     private String priority;
     private boolean isComplete;
+    private List<String> dependencies; 
 
-    public Task(String title, String description, String dueDate, String category, String priority, boolean isComplete) {
+    public Task(String title, String description, String dueDate, String category, String priority, boolean isComplete, List<String> dependencies) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.category = category;
         this.priority = priority;
         this.isComplete = isComplete;
+        this.dependencies = dependencies;
+
     }
 
     public String getTitle() { return title; }
@@ -32,6 +39,10 @@ public class Task {
 
     public boolean isComplete() { return isComplete; }
     public void setComplete(boolean isComplete) { this.isComplete = isComplete; }
+    
+    public List<String> getDependencies() { return dependencies; }
+
+    public void setDependencies(List<String> dependencies) { this.dependencies = dependencies; }
 
     public int getDueDateAsInteger() {
         try {
@@ -42,11 +53,17 @@ public class Task {
         }
     }
 
-    @Override
+   @Override
     public String toString() {
         return String.format(
-            "[%s] %s | %s - Due: %s - Category: %s - Priority: %s",
-            isComplete ? "Complete" : "Incomplete", title, description, dueDate, category,priority);
+            "[%s] %s | %s - Due: %s - Category: %s - Priority: %s - Dependencies: %s",
+            isComplete ? "Complete" : "Incomplete", 
+            title, 
+            description, 
+            dueDate, 
+            category, 
+            priority, 
+            dependencies.isEmpty() ? "None" : String.join(", ", dependencies));
     }
 
 }
